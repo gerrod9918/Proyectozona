06466e00c0aced678604257dd1a04f3286513ddc
gerrod9918 <germanrodriguez1@hotmail.es>
2019-11-04 03:55:11 -0600

    Creacion de vista de paciente con sus funciones2

--- notificaciones/settings.py
+++ notificaciones/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'notificaciones.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['.templates/'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,5 +120,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-LOGIN_REDIRECT_URL = '/'
